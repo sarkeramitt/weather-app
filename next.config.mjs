9846b5952e4c4b1d0320b15ecda03ae772b59b8e
sarkeramitt <amitsarker.me@gmail.com>
2025-03-03 17:57:00 +0600

    Initial commit: Update Next.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ['cdn.weatherapi.com'],
+      unoptimized: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    experimental: {
+      webpackBuildWorker: true,
+      parallelServerBuildTraces: true,
+      parallelServerCompiles: true,
+    },
+  }
+  
+  export default nextConfig
+  
+  
